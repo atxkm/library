ae73330477568761f50342b1dc192fd88f588072
TaNg <tangxiaokui@126.com>
2016-04-14 11:54:50 +0800

    update

--- keyword-test.js
+++ keyword-test.js
@@ -1,20 +1,20 @@
 /**
  * 注释信息关键字测试
- * @param  {Number}
- * @param  {number}
- * @return {String}
- * @return {string}
- * @return {Array}
- * @return {array}
- * @return {Object}
- * @return {object}
- * @return {Boolean}
- * @return {boolean}
- * @return {JSON}
- * @return {json}
- * @author
- * @datetime
- * @datetime
- * @date
- * @time
+ * @param  {Number} aa	数字
+ * @param  {number} aa	数字
+ * @return {String} aa	数字
+ * @return {string} aa	数字
+ * @return {Array} aa	数字
+ * @return {array} aa	数字
+ * @return {Object} aa	数字
+ * @return {object} aa	数字
+ * @return {Boolean} aa	数字
+ * @return {boolean} aa	数字
+ * @return {JSON} aa	数字
+ * @return {json} aa	数字
+ * @author aa	数字
+ * @datetime aa	数字
+ * @datetime aa	数字
+ * @date aa	数字
+ * @time aa	数字
  */
