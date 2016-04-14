97ca6f8bd0e0faf4edb7a13fa061377f57a06ca2
TaNg <tangxiaokui@126.com>
2016-04-14 11:52:29 +0800

    关键字测试

---
+++ keyword-test.js
@@ -0,0 +1,20 @@
+/**
+ * 注释信息关键字测试
+ * @param  {Number}
+ * @param  {number}
+ * @return {String}
+ * @return {string}
+ * @return {Array}
+ * @return {array}
+ * @return {Object}
+ * @return {object}
+ * @return {Boolean}
+ * @return {boolean}
+ * @return {JSON}
+ * @return {json}
+ * @author
+ * @datetime
+ * @datetime
+ * @date
+ * @time
+ */
